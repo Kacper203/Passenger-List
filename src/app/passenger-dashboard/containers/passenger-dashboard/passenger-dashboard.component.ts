8ed138afd32e6ec724f36f481d18a13b3e8d4556
Kacper203 <96740243+Kacper203@users.noreply.github.com>
2022-05-29 11:26:43 +0200

    add name editor

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -47,13 +47,21 @@ export class PassengerDashboardComponent implements OnInit {
     }
 
     handleEdit(event: Passenger) {
-      this.passengers = this.passengers.map((passenger: Passenger) => {
-        if (passenger.id === event.id) {
-          passenger = Object.assign({}, passenger, event);
-        }
-        return passenger;
-      });
-    }
+      this.passengerService.uptadePassenger(event).subscribe((data: Passenger[]) =>{
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+      })
+    //   this.passengers = this.passengers.map((passenger: Passenger) => {
+    //     if (passenger.id === event.id) {
+    //       passenger = Object.assign({}, passenger, event);
+    //     }
+    //     return passenger;
+    //   });
+    // }
+      })}
     handleRemove(event: Passenger) {
       this.passengers = this.passengers.filter((passenger: Passenger) => {
         return passenger.id !== event.id;
--- src/app/passenger-dashboard/model/passenger-dashboard.servis.ts
+++ src/app/passenger-dashboard/model/passenger-dashboard.servis.ts
@@ -6,11 +6,19 @@ import { map, Observable, tap } from "rxjs";
 const PASENGER_API: string = '/assets/db.json' 
 @Injectable()
 export class PassengerDaschboarService{
-
+ 
   constructor(private http: HttpClient){}
 
   getPassengers(): Observable<Passenger[]> {
     return this.http.get<PassangerResponse>(PASENGER_API)
+      .pipe(
+        map((response: PassangerResponse) => {
+          return response.passengers;
+        })
+      )
+  }
+ uptadePassenger(passenger: Passenger): Observable<Passenger[]> {
+    return this.http.get<PassangerResponse>(`${PASENGER_API}/${passenger.id}`)
       .pipe(
         map((response: PassangerResponse) => {
           return response.passengers;
